893ef6e33db5d9d06e870c3a2ce460fd10243065
Александр Белькевич <alexbelkevich@MacBook-Pro-Aleksandr.local>
2024-03-01 23:08:51 +0400

    Start project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {computed} from 'vue'
+const test = computed<string>(() => 'test')
+import TheWelcome from '@/components/TheWelcome.vue'
+
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
